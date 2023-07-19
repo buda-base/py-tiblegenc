8f907aa0d662881519cba2e245b563befff82246
eroux <elie.roux@telecom-bretagne.eu>
2023-07-19 10:13:37 +0200

    use cropbox instead of mediabox

--- deduff_pdf.py
+++ deduff_pdf.py
@@ -8,8 +8,11 @@ from pdfminer_text_converter import DuffedTextConverter
 from pdfminer.pdfdocument import PDFDocument
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.pdfpage import PDFPage
+from pdfminer.layout import LTPage
 from pdfminer.pdfparser import PDFParser
 from pdfminer.layout import LAParams
+from pdfminer.converter import PDFLayoutAnalyzer
+from pdfminer.utils import apply_matrix_pt
 
 # uncomment to debug region
 #logging.basicConfig(level=logging.DEBUG)
@@ -18,6 +21,38 @@ from pdfminer.layout import LAParams
 # REGION = [132,0,928,100000] # KWSB
 #REGION = [125,0,935,100000] # KWKB
 REGION = [0,0,100000,100000] # KWKB
+
+# see https://github.com/pdfminer/pdfminer.six/issues/900
+# for some reason pdfminer uses the mediabox coordinates for LTPage
+# but it seems that cropbox is a better choice. This hack does this:
+
+def cropbox_begin_page(self, page, ctm):
+    print("cropbox: %s" % page.cropbox)
+    print(ctm)
+    (x0, y0, x1, y1) = page.cropbox
+    (x0, y0) = apply_matrix_pt(ctm, (x0, y0))
+    (x1, y1) = apply_matrix_pt(ctm, (x1, y1))
+    mediabox = (0, 0, abs(x0 - x1), abs(y0 - y1))
+    self.cur_item = LTPage(self.pageno, mediabox)
+
+def cropbox_process_page(self, page: PDFPage) -> None:
+        #log.debug("Processing page: %r", page)
+        (x0, y0, x1, y1) = page.cropbox
+        if page.rotate == 90:
+            ctm = (0, -1, 1, 0, -y0, x1)
+        elif page.rotate == 180:
+            ctm = (-1, 0, 0, -1, x1, y1)
+        elif page.rotate == 270:
+            ctm = (0, 1, -1, 0, y1, -x0)
+        else:
+            ctm = (1, 0, 0, 1, -x0, -y0)
+        self.device.begin_page(page, ctm)
+        self.render_contents(page.resources, page.contents, ctm=ctm)
+        self.device.end_page(page)
+        return
+
+PDFLayoutAnalyzer.begin_page = cropbox_begin_page
+PDFPageInterpreter.process_page = cropbox_process_page
 
 def deduffed_txt_from_pdf(pdf_file_name, region=None, page_break_str="\n\n-- page {} --\n\n", remove_non_hz=True):
     stats = {
