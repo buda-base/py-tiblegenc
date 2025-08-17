ab6ee4aa74a79fa92626d6c0fa10f9c7f51b458f
Elie Roux <roux.elie@gmail.com>
2025-08-17 16:44:36 +0200

    WIP: font name normalization, make stats optional, stronger uni / cp1252 conv

--- pytiblegenc/char_converter.py
+++ pytiblegenc/char_converter.py
@@ -29,15 +29,21 @@ def get_base_from_file(filename):
         for row in reader:
             if row[0] not in base:
                 base[row[0]] = {}
-            base[row[0]][chr(int(row[1]))] = row[2]
+            unicp = int(row[1])
+            if unicp < 256:
+                unicpfromcp1252 = uni_char_from_encoding(unicp)
+                if unicpfromcp1252:
+                    if chr(unicpfromcp1252) not in base[row[0]]:
+                        base[row[0]][chr(unicpfromcp1252)] = row[2]            
+            base[row[0]][chr(unicp)] = row[2]
     return base
 
 FONT_ALIASES = {
     "Dedris-syma": "Ededris-sym",
     "Ededris-syma": "Ededris-sym",
     "TibetanClassicSkt": "TibetanClassicSkt1",
-    "TibetanChogyalSkt": "TibetanChogyalSkt1",
-}
+    "TibetanChogyalSkt": "TibetanChogyalSkt1"
+    }
 
 def uni_char_from_encoding(nonunicp, encoding="cp1252"):
     noncpbytes = nonunicp.to_bytes(1, "big")
@@ -47,7 +53,10 @@ def uni_char_from_encoding(nonunicp, encoding="cp1252"):
     except UnicodeDecodeError:
         return
 
-def _convert_char(char, font_name, stats):
+def _convert_char(char, font_name, stats=None):
+    """
+    /!\ font_name doesn't get normalized in this function
+    """
     base = get_base()
     utfc_base = get_utfc_base()
     if char == "\u00a0":
@@ -55,39 +64,50 @@ def _convert_char(char, font_name, stats):
     base_ft = base.get(font_name)
     utfc_base_ft = utfc_base.get(font_name)
     if (base_ft is None or char not in base_ft) and (utfc_base_ft is None or char not in utfc_base_ft):
-        if font_name not in stats["unknown_characters"]:
-            stats["unknown_characters"][font_name] = {}
-        if char not in stats["unknown_characters"][font_name]:
-            stats["unknown_characters"][font_name][char] = 0
-        stats["unknown_characters"][font_name][char] += 1
+        if stats:
+            if font_name not in stats["unknown_characters"]:
+                stats["unknown_characters"][font_name] = {}
+            if char not in stats["unknown_characters"][font_name]:
+                stats["unknown_characters"][font_name][char] = 0
+            stats["unknown_characters"][font_name][char] += 1
         #logging.error("unknown character: '%s' (%d) in %s" % (char, ord(char), font_name))
         if DEBUGMODE:
             #return "%s,%d,?(%s)" % (font_name, ord(char), char)
-            return "[[%s]]" % (char)
+            return "[[%s%s]]" % (font_name, char)
         else:
             return ""
     res = base_ft.get(char) if base_ft is not None else None
     utfc_res = utfc_base_ft.get(char) if utfc_base_ft is not None else None
     if res is not None and utfc_res is not None and res != utfc_res:
-        stats_key = "%s,%d" % (font_name, ord(char))
-        if stats_key not in stats["diffs_with_utfc"]:
-            stats["diffs_with_utfc"][stats_key] = 0
-        stats["diffs_with_utfc"][stats_key] += 1
+        if stats:
+            stats_key = "%s,%d" % (font_name, ord(char))
+            if stats_key not in stats["diffs_with_utfc"]:
+                stats["diffs_with_utfc"][stats_key] = 0
+            stats["diffs_with_utfc"][stats_key] += 1
         if DEBUGMODE:
             return "[[%s,%d,%s or %s]]" % (font_name, ord(char), res, utfc_res)
         else:
             return res
     if res == ERROR_CHR:
-        stats["error_characters"] += 1
+        if stats:
+            stats["error_characters"] += 1
         if DEBUGMODE:
             return '[[ERR]]'
         else:
             return ''
     return res if res is not None else utfc_res
 
-def convert_string(s, font_name, stats):
-    if s.startswith("(cid:"):
-        return ""
+def _convert_byte(b, font_name, stats=None):
+    """
+    b is expected to be a number between 0 and 255
+    """
+    unic = uni_char_from_encoding(b)
+    return _convert_char(unic, font_name, stats)
+
+def normalize_font_name(font_name, weight=None):
+    """
+    weight is "b", "i" or "bi"
+    """
     if font_name in FONT_ALIASES:
         font_name = FONT_ALIASES[font_name]
     if font_name.startswith("Dedris"):
@@ -95,6 +115,21 @@ def convert_string(s, font_name, stats):
     # Todo: also replace "Drutsa-" and "Khamdris-" to "Ededris-"
     if font_name.startswith("Sam") and len(font_name) == 4:
         font_name = "Es"+font_name[1:]
+    if font_name.endswith("Normal"):
+        font_name = font_name[:-6].strip()
+    # old conventions for WordPerfect
+    if weight == "b":
+        font_name += "Skt1"
+    if weight == "i":
+        font_name += "Skt2"
+    if weight == "bi":
+        font_name += "Skt3"
+    return font_name
+
+def convert_string(s, font_name, stats):
+    if s.startswith("(cid:"):
+        return ""
+    font_name = normalize_font_name(font_name)
     base = get_base()
     utfc_base = get_utfc_base()
     if font_name not in base and font_name not in utfc_base:
